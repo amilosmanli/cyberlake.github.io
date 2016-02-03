96d61e4f4c20460b2e1fec15a94d5eb3de683784
Amil Osmanli <osmanli.ao@gmail.com>
2016-02-03 17:36:58 +0000

    Yo Yo Yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cyberlakegithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cyberlakegithubioApp
+ */
+angular.module('cyberlakegithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
